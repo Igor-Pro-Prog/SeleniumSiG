07ff57ea93e2c87fb8b7f88eb9f79fec36a6f7f2
Igor Gabriel <97804247+igorUfca@users.noreply.github.com>
2022-09-23 14:50:48 -0300

    Atualização do  Selenium pre-Produção SIGAA

--- exercicio.py
+++ exercicio.py
@@ -1,25 +1,37 @@
+from getpass import getpass
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from time import sleep
 
-import json
-
-with open("exercicio.json", "r") as arquivo:
-    pei=json.load(arquivo)
 
 driver = webdriver.Firefox()
 
+driver.get("https://preproducao-esig.ufca.edu.br/sigaa/public/home.jsf")
 
-driver.get(pei["SiteUFRN"])
+sleep (2)
 
-usuario="123"
-senha="123"
+#cria um loop para verificar se o usuario esta logado pela url
 
-user = driver.find_element(By.NAME, pei["inputNome"])
-user.send_keys(usuario)
+while driver.current_url != "https://preproducao-esig.ufca.edu.br/sigaa/graduacao/geral.jsf":
+    sleep(2)
+    print("Aguardando login")
+    sleep(2)
+    # enquanto for diferente do link da pagina de graduação
 
-senha = driver.find_element(By.NAME, pei["inputSenha"])
-senha.send_keys(senha)
+driver.find_element(By.LINK_TEXT, "Atualizar Dados Pessoais" ).click()
 
-botao = driver.find_element(By.XPATH, pei["xpathBotaoEntrar"])
-botao.click()
+sleep(2)
+
+driver.find_element(By.ID, "formulario:matriculaDiscente" ).click()
+
+sleep(2)
+
+# Digita a matricula pelo terminal  
+matricula = getpass("Digite a matricula: ")
+# clica em continuar
+driver.find_element(By.ID, "formulario:matriculaDiscente" ).send_keys(matricula)
+#clica em buscar
+driver.find_element(By.ID, "formulario:buscar" ).click()
+#seleciona o discente
+driver.find_element(By.ID, "form:selecionarDiscente" ).click()
